9175c1b0f535165afdc5d644eb3d33d9247cf2ed
William <wsmedina@espol.edu.ec>
2021-05-23 23:32:18 -0500

    First commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor'];
+  heroeBorrado:string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.pop() ||'';
+  }
+
+}
